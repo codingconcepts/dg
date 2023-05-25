50bb6e5358247492fc65339c48bbff742b601cfc
codingconcepts <hello@robreid.io>
2023-05-25 09:45:43 +0100

    feat: Test for cartesian product

--- go.mod
+++ go.mod
@@ -5,7 +5,12 @@ go 1.20
 require (
 	github.com/brianvoe/gofakeit/v6 v6.21.0
 	github.com/samber/lo v1.38.1
+	github.com/stretchr/testify v1.8.3
 	gopkg.in/yaml.v3 v3.0.1
 )
 
-require golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+)
---
+++ internal/pkg/generator/array_test.go
@@ -0,0 +1,50 @@
+package generator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCartesianProduct(t *testing.T) {
+	cases := []struct {
+		name   string
+		input  [][]string
+		output [][]string
+	}{
+		{
+			name: "single input",
+			input: [][]string{
+				{"a", "b", "c"},
+			},
+			output: [][]string{
+				{"a"}, {"b"}, {"c"},
+			},
+		},
+		{
+			name: "multiple input",
+			input: [][]string{
+				{"a", "b", "c"},
+				{"d", "e", "f"},
+			},
+			output: [][]string{
+				{"a", "d"},
+				{"b", "d"},
+				{"c", "d"},
+				{"a", "e"},
+				{"b", "e"},
+				{"c", "e"},
+				{"a", "f"},
+				{"b", "f"},
+				{"c", "f"},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			actual := CartesianProduct(c.input...)
+			assert.Equal(t, c.output, actual)
+		})
+	}
+}
