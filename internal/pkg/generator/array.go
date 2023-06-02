84f649a351d5352b7fcbebdd5cfbbb1440e3fecc
codingconcepts <hello@robreid.io>
2023-06-02 10:05:53 +0100

    feat: Faster Cartesian product

--- internal/pkg/generator/array.go
+++ internal/pkg/generator/array.go
@@ -1,19 +1,39 @@
 package generator
 
+import (
+	"github.com/samber/lo"
+)
+
 // CartesianProduct returns the Cartesian product of a variable number of arrays.
-func CartesianProduct(a ...[]string) (c [][]string) {
+func CartesianProduct(a ...[]string) [][]string {
 	if len(a) == 0 {
-		return [][]string{nil}
+		return [][]string{}
 	}
 
-	last := len(a) - 1
-	l := CartesianProduct(a[:last]...)
-	for _, e := range a[last] {
-		for _, p := range l {
-			c = append(c, append(p, e))
+	totalCombinations := lo.Reduce(a, func(agg int, item []string, index int) int {
+		return agg * len(item)
+	}, 1)
+
+	// Preallocate the result slice with the correct capacity.
+	result := make([][]string, 0, totalCombinations)
+	result = append(result, []string{})
+
+	// Generate the Cartesian products.
+	for _, arr := range a {
+		temp := make([][]string, 0, totalCombinations)
+		for _, element := range arr {
+			for _, combination := range result {
+				// Create a new combination by appending the current element
+				newCombination := make([]string, len(combination)+1)
+				copy(newCombination, combination)
+				newCombination[len(combination)] = element
+				temp = append(temp, newCombination)
+			}
 		}
+		result = temp
 	}
-	return
+
+	return result
 }
 
 // Transpose a multi-dimensional array.
--- internal/pkg/generator/array_test.go
+++ internal/pkg/generator/array_test.go
@@ -1,6 +1,7 @@
 package generator
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -37,6 +38,36 @@ func TestCartesianProduct(t *testing.T) {
 				{"a", "f"},
 				{"b", "f"},
 				{"c", "f"},
+			},
+		},
+		{
+			name: "small array big array",
+			input: [][]string{
+				{"a", "b"},
+				{"d", "e", "f"},
+			},
+			output: [][]string{
+				{"a", "d"},
+				{"b", "d"},
+				{"a", "e"},
+				{"b", "e"},
+				{"a", "f"},
+				{"b", "f"},
+			},
+		},
+		{
+			name: "big array small array",
+			input: [][]string{
+				{"a", "b", "c"},
+				{"d", "e"},
+			},
+			output: [][]string{
+				{"a", "d"},
+				{"b", "d"},
+				{"c", "d"},
+				{"a", "e"},
+				{"b", "e"},
+				{"c", "e"},
 			},
 		},
 	}
@@ -82,6 +113,107 @@ func TestTranspose(t *testing.T) {
 		t.Run(c.name, func(t *testing.T) {
 			actual := Transpose(c.input)
 			assert.Equal(t, c.output, actual)
+		})
+	}
+}
+
+func BenchmarkCartesianProduct(b *testing.B) {
+	cases := []struct {
+		name  string
+		input [][]string
+	}{
+		{
+			name: "single array",
+			input: [][]string{
+				{"a", "b", "c"},
+			},
+		},
+		{
+			name: "two small arrays",
+			input: [][]string{
+				{"a", "b", "c"},
+				{"d", "e", "f"},
+			},
+		},
+		{
+			name: "three small arrays",
+			input: [][]string{
+				{"a", "b", "c"},
+				{"d", "e", "f"},
+				{"g", "h", "i"},
+			},
+		},
+		{
+			name: "small array and big array",
+			input: [][]string{
+				{"a", "b", "c"},
+				strings.Split(strings.Repeat("d", 1000), ""),
+			},
+		},
+		{
+			name: "big array and small array",
+			input: [][]string{
+				strings.Split(strings.Repeat("a", 1000), ""),
+				{"d", "e", "f"},
+			},
+		},
+		{
+			name: "big arrays",
+			input: [][]string{
+				strings.Split(strings.Repeat("a", 1000), ""),
+				strings.Split(strings.Repeat("d", 1000), ""),
+			},
+		},
+	}
+
+	for _, c := range cases {
+		b.Run(c.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				CartesianProduct(c.input...)
+			}
+		})
+	}
+}
+
+func BenchmarkTranspose(b *testing.B) {
+	cases := []struct {
+		name  string
+		input [][]string
+	}{
+		{
+			name: "single array",
+			input: [][]string{
+				{"a", "b", "c"},
+			},
+		},
+		{
+			name: "multiple small arrays",
+			input: [][]string{
+				{"a", "b", "c"},
+				{"d", "e", "f"},
+			},
+		},
+		{
+			name: "small array and big array",
+			input: [][]string{
+				{"a", "b", "c"},
+				strings.Split(strings.Repeat("d", 1000), ""),
+			},
+		},
+		{
+			name: "big array and small array",
+			input: [][]string{
+				strings.Split(strings.Repeat("a", 1000), ""),
+				{"d", "e", "f"},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		b.Run(c.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				CartesianProduct(c.input...)
+			}
 		})
 	}
 }
